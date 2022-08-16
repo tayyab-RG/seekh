a527d94ce50a16c1199c087b6be5b0496ea90ffa
Tayyab Nadeem <tayyab@revolvinggames.com>
2022-08-16 18:19:04 +0500

    Code Review: move checks outside router

--- backend/dist/controllers/user.js
+++ backend/dist/controllers/user.js
@@ -8,53 +8,134 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateUser = exports.deleteUser = exports.getAllUsers = exports.createUser = void 0;
+exports.getUser = exports.updateUser = exports.deleteUser = exports.getAllUsers = exports.createUser = void 0;
 const client_1 = require("@prisma/client");
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
 const prisma = new client_1.PrismaClient();
-function createUser(name, email) {
+function createUser(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
-        const user = yield prisma.user.create({
-            data: {
-                name: name,
+        let { name, email, password } = req.body;
+        if (!name)
+            return res.status(400).json({ success: false, msg: 'Name cannot be empty!' });
+        if (!email)
+            return res.status(400).json({ success: false, msg: 'Email cannot be empty!' });
+        if (!password)
+            return res.status(400).json({ success: false, msg: 'Password cannot be empty!' });
+        // Check if email already exists
+        const oldUser = yield prisma.user.findFirst({
+            where: {
                 email: email,
-            },
-            select: {
-                id: true,
-                name: true,
-                email: true
             }
         });
-        return user;
+        if (oldUser) {
+            return res.status(409).send("User Already Exist!");
+        }
+        const encryptedPassword = yield bcryptjs_1.default.hash(password, 10);
+        try {
+            const user = yield prisma.user.create({
+                data: {
+                    name: name,
+                    email: email,
+                    password: encryptedPassword
+                },
+                select: {
+                    id: true,
+                    name: true,
+                    email: true,
+                    password: true
+                }
+            });
+            res.status(201).json({ success: true, msg: "User added.", data: user });
+        }
+        catch (error) {
+            res.status(500).json({ success: false, msg: 'Something went wrong!' });
+        }
     });
 }
 exports.createUser = createUser;
-function getAllUsers() {
+function getAllUsers(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
-        const users = yield prisma.user.findMany();
-        return users;
+        try {
+            const users = yield prisma.user.findMany();
+            res.status(200).json({ data: users });
+        }
+        catch (err) {
+            res.status(500).json({ msg: "Something Went wrong!" });
+        }
     });
 }
 exports.getAllUsers = getAllUsers;
-function deleteUser(id) {
-    if (!id)
-        return;
-    return prisma.user.delete({
-        where: {
-            id: id,
-        },
+function deleteUser(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { id } = req.params;
+        if (!id)
+            res.status(400).json({ msg: "Id is required!" });
+        try {
+            let updatedPerson = yield prisma.user.delete({
+                where: {
+                    id: id,
+                },
+            });
+            res.status(200).json({ success: true, data: updatedPerson });
+        }
+        catch (error) {
+            console.log(error);
+            res.status(500).json({ success: false, msg: 'Something went wrong!' });
+        }
     });
 }
 exports.deleteUser = deleteUser;
-function updateUser(id, name, email) {
-    return prisma.user.update({
-        where: {
-            id: id,
-        },
-        data: {
-            name: name,
-            email: email
-        },
+function updateUser(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { id } = req.params;
+        const { name, email, password } = req.body;
+        if (!name)
+            return res.status(400).json({ success: false, msg: 'Name cannot be empty!' });
+        if (!email)
+            return res.status(400).json({ success: false, msg: 'Email cannot be empty!' });
+        if (!password)
+            return res.status(400).json({ success: false, msg: 'Password cannot be empty!' });
+        try {
+            let updatedPerson = yield prisma.user.update({
+                where: {
+                    id: id,
+                },
+                data: {
+                    name: name,
+                    email: email,
+                    password: password
+                },
+            });
+            res.status(200).json({ success: true, data: updatedPerson });
+        }
+        catch (error) {
+            console.log(error);
+            res.status(500).json({ success: false, msg: 'Something went wrong!' });
+        }
     });
 }
 exports.updateUser = updateUser;
+function getUser(req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { id } = req.params;
+        if (!id)
+            res.status(400).json({ msg: "Id is required!" });
+        try {
+            const user = yield prisma.user.findUnique({
+                where: {
+                    id: id,
+                },
+            });
+            res.status(200).json({ data: user });
+        }
+        catch (error) {
+            console.log(error);
+            res.status(404).json({ msg: "User with given Id not found!" });
+        }
+    });
+}
+exports.getUser = getUser;
--- backend/dist/routes/user.js
+++ backend/dist/routes/user.js
@@ -1,62 +1,13 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const user_1 = require("../controllers/user");
-router.post('/user/create', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    let { name, email } = req.body;
-    if (!name)
-        return res.status(400).json({ success: false, msg: 'Name cannot be emoty!' });
-    if (!email)
-        return res.status(400).json({ success: false, msg: 'Email cannot be emoty!' });
-    try {
-        const user = yield (0, user_1.createUser)(name, email);
-        res.status(201).json({ success: true, msg: "person added.", data: user });
-    }
-    catch (error) {
-        res.status(500).json({ success: false, msg: 'Something went wrong!' });
-    }
-}));
-router.get('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const users = yield (0, user_1.getAllUsers)();
-    res.status(200).json({ data: users });
-}));
-router.delete('/user/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { id } = req.params;
-    try {
-        let updatedPerson = yield (0, user_1.deleteUser)(id);
-        res.status(200).json({ success: true, data: updatedPerson });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ success: false, msg: 'Something went wrong!' });
-    }
-}));
-router.put('/user/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { name, email } = req.body;
-    if (!name)
-        return res.status(400).json({ success: false, msg: 'Name cannot be emoty!' });
-    if (!email)
-        return res.status(400).json({ success: false, msg: 'Email cannot be emoty!' });
-    const { id } = req.params;
-    try {
-        let updatedPerson = yield (0, user_1.updateUser)(id, name, email);
-        res.status(200).json({ success: true, data: updatedPerson });
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({ success: false, msg: 'Something went wrong!' });
-    }
-}));
+router.post('/user/create', user_1.createUser);
+router.get('/users', user_1.getAllUsers);
+router.get('/user/:id', user_1.getUser);
+router.delete('/user/:id', user_1.deleteUser);
+router.put('/user/:id', user_1.updateUser);
 module.exports = router;
