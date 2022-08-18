e15a63dd88377a9b2205d5e69379813e8f56435b
Tayyab Nadeem <tayyab@revolvinggames.com>
2022-08-18 12:34:18 +0500

    create, delete, read course

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -7,6 +7,7 @@ import prisma from '../prisma';
 dotenv.config();
 
 export async function userSignup(req: Request, res: Response) {
+    console.log(req.body);
     let { name, email, password } = req.body;
 
     if (!name) return res.status(400).json({ success: false, msg: 'Name cannot be empty!' });
--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes'
 import authRoutes from './routes/authRoutes'
+import courseRouter from './routes/courseRoutes'
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use(express.json());
 
 app.use('/', userRoutes);
 app.use('/', authRoutes);
+app.use('/', courseRouter);
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
